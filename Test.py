31d35d0566c417e2da522d6e9b75db7716850e45
chloetriestocomputer <63759587+chloetriestocomputer@users.noreply.github.com>
2020-06-15 16:19:58 -0400

    Chloe did the thing

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ def shubham_says():
 	pass # shubham - pull and fill this in!
 
 def chloe_says():
-	pass # chloe - pull and fill this in!
+	print("According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways. Because bees don't care what humans think is impossible.")
 
 def catherine_says():
-        print("vim is hard, that's what she said")
+    print("vim is hard, that's what she said")
