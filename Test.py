45215c05751f0312b3b3a0d7af46b8218e8b8e01
Max Daniels <qnkxsovc@gmail.com>
2020-06-15 15:12:39 -0400

    added pull test file

---
+++ Test.py
@@ -0,0 +1,12 @@
+
+def max_says():
+	print("It's a-me, Max")
+
+def shubham_says():
+	pass # shubham - pull and fill this in!
+
+def chloe_says():
+	pass # chloe - pull and fill this in!
+
+def catherine_says():
+	pass # catherine - pull and fill this in!
