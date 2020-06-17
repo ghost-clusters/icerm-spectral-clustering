90811a54a95f2bf7babd71e9622e6b566ad37baf
chloetriestocomputer <63759587+chloetriestocomputer@users.noreply.github.com>
2020-06-17 15:07:14 -0400

    saving Shubham from himself

--- unnormalized_spectral_clustering.py
+++ unnormalized_spectral_clustering.py
@@ -15,19 +15,18 @@ def unnormalized_spectral_clustering(data, k): # data is a list of points in R_2
 	# 7. output cluster
 	laplacian = laplacian_matrix(data)
 	u_first_k_evectors = sp.linalg.eigh(laplacian, eigvals=(0, k-1))[1]
-	print("here")
-	make_plot(u_first_k_evectors)
+	#make_plot(u_first_k_evectors)
 	for i in range(len(data)):
 		#convert arrays to points
 		u_first_k_evectors[i]
-
 	clusters, assns = kmeans(u_first_k_evectors, k)
+	make_plot(clusters)
 
 def gen_random_points(number, length):
 	l = []
 	for i in range(number):
 		l.append(10.0*np.random.rand(length))
-	print(l)
+	#print(l)
 	return l 
 
 def laplacian_matrix(data):
@@ -49,7 +48,9 @@ def make_plot(matrix):
 	newmatrix = np.transpose(matrix)
 	x = newmatrix[0]
 	y = newmatrix[1]
+	print("X: ", x)
+	print("Y: ", y)
 	plt.plot(x,y,'ro')
 	plt.show()
 
-unnormalized_spectral_clustering(gen_random_points(4, 2), 2)
+unnormalized_spectral_clustering(gen_random_points(50, 10), 2)
