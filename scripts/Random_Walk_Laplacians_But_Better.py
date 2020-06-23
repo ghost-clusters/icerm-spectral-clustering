86213b5998f63179c90cf5431abc66583f81e88f
chloetriestocomputer <63759587+chloetriestocomputer@users.noreply.github.com>
2020-06-23 11:13:04 -0400

    made random walk laplacians but better better

--- scripts/Random_Walk_Laplacians_But_Better.py
+++ scripts/Random_Walk_Laplacians_But_Better.py
@@ -10,13 +10,19 @@ from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
 
 # ## Clusting based off of tranistion matrix
-def random_walk_clustering(numOfTrials, data, shape):
+def random_walk_clustering(numOfTrials,data):
+    # Making the transition matrix
+    weights = similarity_matrix(data)
+    laplacian, degreeinv = laplacian_matrix(data)
+    transition = degreeinv @ weights
+    shape , _ = transition.shape
     assignedQuestionMark = np.zeros(len(data)) # array with values 0 or 1 to determine whether a vertex has already been assigned a cluster (1) or not (0)
     assignments = np.zeros(len(data)) # index of vertex stores cluster number
-    maxIt = ((len(data))**2) * 15 # max number of iterations of random walk for a given cluster; adjust constant on side if desired
+    maxIt = ((len(data))**2) * 2000 # max number of iterations of random walk for a given cluster; adjust constant on side if desired
     clusterAssign = 0 
     while np.sum(assignedQuestionMark).astype(int) != len(data): # loop runs until every vertex is assigned a cluster
         i = np.random.randint(shape) # picking random vetex to start at
+        # maybe don't do this; instead pick vertices with highest degree first
         while assignedQuestionMark[i] != 0: # ensuring random vertex isn't already in a cluster
             i = np.random.randint(shape)
         cumulativeFreq = np.zeros(len(data)) # array to store the number of times a vertex has been visited after numOfTrials trials
@@ -44,7 +50,7 @@ def random_walk_clustering(numOfTrials, data, shape):
                 if cumulativeFreq[l] >= avgFreq:
                     assignedQuestionMark[l] = 1
                     assignments[l] = clusterAssign
-    return(assignments, clusterAssign)
+    return(assignments, clusterAssign,cumulativeFreq,avgFreq)
 
 
 # ## Data Generation
@@ -55,6 +61,7 @@ n = n_pts * n_gaussians
 d = 2
 
 data = gaussian_mixture(n_gaussians, n_pts, d, centroid_var=15)
+data = data.T
 
 # plt.scatter(*data)
 # plt.xlabel("$x_1$")
@@ -62,18 +69,15 @@ data = gaussian_mixture(n_gaussians, n_pts, d, centroid_var=15)
 # plt.title("Sample of Gaussian Mixture")
 # plt.show()
 
-# Making the transition matrix
-data = data.T
-
-weights = similarity_matrix(data)
-laplacian, degreeinv = laplacian_matrix(data)
-
-transition = degreeinv @ weights
-shape , _ = transition.shape
-
 # calling fn and plotting
 numOfTrials = 1000
-assignments,clusterAssign = random_walk_clustering(numOfTrials,data,shape)
+assignments,clusterAssign, cumulative, avg = random_walk_clustering(numOfTrials,data)
+
+# print(avg)
+# print(cumulative)
+# plt.hist(cumulative, bins = n )
+# plt.show()
+
 print(assignments)
 make_plot(data,assignments,clusterAssign+1)
 plt.show()
