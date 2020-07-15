675cdf5ac1fb5669d8c23377eb436b3c3714a93d
Shubham Makharia <shubham_makharia@brown.edu>
2020-07-15 12:47:37 -0400

    smaller mushroom graph for tweakin

--- lib/spectral_clustering.py
+++ lib/spectral_clustering.py
@@ -40,6 +40,7 @@ def similarity_matrix(data, s=1, metric="g", kernel=None, numOfAtts=None):
                 similarity_matrix[i][j] = kernel(data[i], data[j], s)
     if(metric == "eskin"):
         similarity_matrix = csf.eskin_similarity(numOfAtts,data)
+        similarity_matrix = csf.shrink_eskin(similarity_matrix, 100) #to be modularized @max
     return similarity_matrix
 
 def laplacian_matrix(graph_weights):
